7a28826c326b6c9ca5fdf7121f4334691fe1a3bd
aluselex <asaleksandrova@mail.ru>
2022-02-21 22:57:37 +0300

    6.0.1. Объявление массива. [#3613]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -4,12 +4,12 @@ public class CheckPrimeNumber {
 
     public static boolean check(int number) {
         boolean prime = number > 1;
-            for (int i = 2; i <= number / 2; i++) {
-                if (number % i == 0) {
-                    prime = false;
-                    break;
-                }
+        for (int i = 2; i <= number / 2; i++) {
+            if (number % i == 0) {
+                prime = false;
+                break;
             }
+        }
         return prime;
     }
 
